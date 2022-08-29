a520011a83140e2fdb5b79d04297775990d4261f
ADIKryvonos <108137330+ADIKryvonos@users.noreply.github.com>
2022-08-29 20:35:29 +0300

    continue

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,12 +4,10 @@
     closeMenuBtn: document.querySelector('.menu-close'),
     menu: document.querySelector('.mob-menu'),
     body: document.querySelector('body'),
-    menuList: document.querySelector('.mob-menu-list'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
-  refs.menuList.addEventListener('click', removeMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
@@ -17,7 +15,4 @@
     refs.body.classList.toggle('no-scroll');
   }
 
-  function removeMenu() {
-    refs.menu.classList.add('is-hidden');
-  }
 })();
